62e6fab3fe5d23db251218be171d924e95907cfb
Yerisal Christofel Lodo <rizallodo@gmail.com>
2018-04-01 19:36:32 +0800

    Mengisi Nilai Atribut

--- src/praktek01/ProdukAirMineralAksi.java
+++ src/praktek01/ProdukAirMineralAksi.java
@@ -14,5 +14,23 @@ public class ProdukAirMineralAksi {
         ProdukAirMineral AQUA = new ProdukAirMineral();
         ProdukAirMineral LeMinerale = new ProdukAirMineral();
         ProdukAirMineral PROF = new ProdukAirMineral();
+        
+        AQUA.merek="AQUA";
+        AQUA.warna="Biru Tua";
+        AQUA.kemasan="Gelas Plastik";
+        AQUA.volume="240 ml";
+        AQUA.kadaluarsa="BB 200619";
+        
+        LeMinerale.merek="Le Minerale";
+        LeMinerale.warna="Biru Langit";
+        LeMinerale.kemasan="Botol Plastik";
+        LeMinerale.volume="600 ml";
+        LeMinerale.kadaluarsa="Ags 2019";
+        
+        PROF.merek="PROF";
+        PROF.warna="Biru Mudah";
+        PROF.kemasan="Botol Plastik";
+        PROF.volume="1500 ml";
+        PROF.kadaluarsa="150120";
     }
 }
