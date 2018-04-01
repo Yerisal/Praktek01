babc3ac9b97f1da7630515b617139143799710c8
Yerisal Christofel Lodo <rizallodo@gmail.com>
2018-04-01 19:15:57 +0800

    Membuat Atribut dan Behavior

--- src/praktek01/ProdukAirMineral.java
+++ src/praktek01/ProdukAirMineral.java
@@ -10,5 +10,23 @@ package praktek01;
  * @author proWindows8.1
  */
 public class ProdukAirMineral {
+    String merek;
+    String warna;
+    String kemasan;
+    String volume;
+    String kadaluarsa;
     
+    void cetakInfo(){
+        System.out.println("Merek Produk        :"+merek);
+        System.out.println("Warna Label         :"+warna);
+        System.out.println("Jenis Kemasan       :"+kemasan);
+        System.out.println("Isi Bersih          :"+volume);
+        System.out.println("Tanggal Kadaluarsa  :"+kadaluarsa);
+        
+        System.out.println("");
+        System.out.println("Mengurangi Dehidrasi");
+        System.out.println("Menambah Cairan Tubuh");
+        System.out.println("Meningkatkan Daya Tahan Tubuh");
+        System.out.println("");
+    }
 }
